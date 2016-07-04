1d0e68793d2ac8cecf49d8ee6e78995d990f05e0
Manan Kalra <manankalra29@gmail.com>
2016-07-05 02:15:56 +0530

    Making it pretty

--- app/src/main/java/io/github/manankalra/courtcounter/MainActivity.java
+++ app/src/main/java/io/github/manankalra/courtcounter/MainActivity.java
@@ -7,49 +7,58 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int scoreA = 0;
+    int scoreB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    int scoreA = 0;
-    int scoreB = 0;
 
-    public void plusThreeA(View view){
+    public void plusThreeA(View view) {
         scoreA += 3;
         displayA(scoreA);
     }
-    public void plusTwoA(View view){
+
+    public void plusTwoA(View view) {
         scoreA += 2;
         displayA(scoreA);
     }
-    public void plusOneA(View view){
+
+    public void plusOneA(View view) {
         scoreA += 1;
         displayA(scoreA);
     }
-    public void plusThreeB(View view){
+
+    public void plusThreeB(View view) {
         scoreB += 3;
         displayB(scoreB);
     }
-    public void plusTwoB(View view){
+
+    public void plusTwoB(View view) {
         scoreB += 2;
         displayB(scoreB);
     }
-    public void plusOneB(View view){
+
+    public void plusOneB(View view) {
         scoreB += 1;
         displayB(scoreB);
     }
-    public void reset(View view){
+
+    public void reset(View view) {
         scoreA = 0;
         scoreB = 0;
         displayA(scoreA);
         displayB(scoreB);
     }
+
     private void displayA(int number) {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.zero_text_view);
         quantityTextView.setText("" + number);
     }
+
     private void displayB(int number) {
         TextView quantityTextViewb = (TextView) findViewById(
                 R.id.zero_text_viewb);
